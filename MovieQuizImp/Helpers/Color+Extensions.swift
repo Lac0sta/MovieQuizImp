17d571b792aced87d4f2198008069ae4b25f8caa
Lac0sta <adv.a.frolov@gmail.com>
2025-11-18 23:56:15 +0100

    Add extensions for Color and Font

---
+++ MovieQuizImp/Helpers/Color+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Extensions.swift
+//  MovieQuizImp
+//
+//  Created by Aleksei Frolov on 18. 11. 2025..
+//
+
+import SwiftUI
+
+extension Color {
+    static let appBackground = Color("ypBackground")
+    static let appBlack = Color("ypBlack")
+    static let appGray = Color("ypGray")
+    static let appGreen = Color("ypGreen")
+    static let appRed = Color("ypRed")
+    static let appWhite = Color("ypWhite")
+}
---
+++ MovieQuizImp/Helpers/Font+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Font+Extensions.swift
+//  MovieQuizImp
+//
+//  Created by Aleksei Frolov on 18. 11. 2025..
+//
+
+import SwiftUI
+
+extension Font {
+    static let ysDisplayBold23 = Font.custom("YSDisplay-Bold", size: 23)
+    static let ysDisplayMedium20 = Font.custom("YSDisplay-Medium", size: 20)
+}
