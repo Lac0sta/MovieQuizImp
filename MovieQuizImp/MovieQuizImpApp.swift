0aea320cf84645536fae1251ce2e9785936dfe0a
Lac0sta <adv.a.frolov@gmail.com>
2025-11-18 12:53:45 +0100

    Initial commit, add .gitignore

---
+++ MovieQuizImp/MovieQuizImpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieQuizImpApp.swift
+//  MovieQuizImp
+//
+//  Created by Aleksei Frolov on 17. 10. 2025..
+//
+
+import SwiftUI
+
+@main
+struct MovieQuizImpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
